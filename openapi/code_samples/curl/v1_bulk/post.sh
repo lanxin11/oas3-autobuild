f3955362d27416a873b61316310751565527bebc
Lanxin Zhao <lzhao@zuora.com>
2022-07-15 15:32:42 +0800

    Remove all manual curl code samples

--- openapi/code_samples/curl/v1_bulk/post.sh
+++
@@ -1,1 +0,0 @@
-curl -X POST -H "Authorization: Bearer f21f017e4724445d8647b1f0de7ed6f1" -F "file=@CreateRevenueSchedules.csv" -F "params="{\"actionType\": \"CreateRevenueSchedule\"}" "https://rest.zuora.com/v1/bulk"
