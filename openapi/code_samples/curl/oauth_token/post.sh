86b93cfb31aae6f7a9ee1dec60f77f8eb932b063
Lanxin Zhao <lzhao@zuora.com>
2022-07-29 16:45:51 +0800

    Update post.sh

--- openapi/code_samples/curl/oauth_token/post.sh
+++ openapi/code_samples/curl/oauth_token/post.sh
@@ -1,1 +1,1 @@
-curl -X POST \n -H "Content-Type: application/x-www-form-urlencoded" \n -d "client_id=b64e42ba-7e1a-4bc6-9146-5e1b420306b5" \n --data-urlencode "client_secret=dOFENLWU193EEoEsWjPZrcjLKVr5OrN1HC9Kqg" \n -d "grant_type=client_credentials" \n "https://rest.zuora.com/oauth/token"
+curl -X POST \r -H "Content-Type: application/x-www-form-urlencoded" \r -d "client_id=b64e42ba-7e1a-4bc6-9146-5e1b420306b5" \r --data-urlencode "client_secret=dOFENLWU193EEoEsWjPZrcjLKVr5OrN1HC9Kqg" \r -d "grant_type=client_credentials" \r "https://rest.zuora.com/oauth/token"
